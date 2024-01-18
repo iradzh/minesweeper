ac78545b877be974b354c44c07e00115e2d69d7d
Iryna Zhebryk <irinazhebrik@gmail.com>
2024-01-18 11:45:11 +0100

    init: project setup

---
+++ src/js/sound.js
@@ -0,0 +1,21 @@
+let isMuted = false;
+
+export function playSound(source) {
+  if (!isMuted) {
+    let audio = new Audio(source);
+    audio.play();
+  }
+}
+
+export function toggleVolume() {
+  if (!volumeButton.classList.contains("clicked")) {
+    isMuted = true;
+    volumeButton.innerText = "unmute 🔈";
+
+    volumeButton.classList.add("clicked");
+  } else {
+    isMuted = false;
+    volumeButton.innerText = "mute 🔈";
+    volumeButton.classList.remove("clicked");
+  }
+}
